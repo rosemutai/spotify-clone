ab23f46fa954cbe2602c03f63d3220af61c88975
rosemutai <chepngetichrose2030@gmail.com>
2022-07-29 23:37:49 +0300

    music genres cards

---
+++ components/MainContent.js
@@ -0,0 +1,13 @@
+const MainContent = () => {
+  return (
+    <div className="bg-zinc-800 md:row-start-2 md:row-end-7 md:col-start-2 md:col-end-13 ">
+        <div className="editor-title md:flex md:justify-between">
+            <h2 className="text-gray-50">Editors Pick</h2>
+            <a className="text-gray-300">SEE ALL</a>
+        </div>
+
+    </div>
+  )
+}
+
+export default MainContent
