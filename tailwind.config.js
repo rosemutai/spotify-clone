7d7f52e638a4c45ff343257fc7ecf6577a5e548f
rosemutai <chepngetichrose2030@gmail.com>
2022-08-13 21:53:11 +0300

    add custom css

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,22 @@ module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{html,js}",
+    "./node_modules/tw-elements/dist/js/**/*.js",
   ],
+
+  plugins: [
+    require('tw-elements/dist/plugin')
+  ],
+
   theme: {
     extend: {
       spacing: {
         '10%': '10%',
         '40%': '40%',
+      },
+      boxShadow: {
+        'top': '0px 20 10px -15px red',
       }
     },
     borderWidth: {
